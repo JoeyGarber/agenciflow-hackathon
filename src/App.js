f71ebe0209e812e8091e6f18a4bcd51ee2e171fc
joeygarber <joseph.z.garber@gmail.com>
2023-03-09 13:35:42 -0500

    Added kanban and todo components
    Also gave them routes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes} from "react-router-dom";
 import { Route } from "react-router";
 
 import Homepage from './components/homepage';
-import SignIn from './components/sign-in';
-import SignUp from './components/sign-up';
+import SignIn from './components/auth/sign-in';
+import SignUp from './components/auth/sign-up';
+import TodoKanban from './components/kanban';
+import TodoList from './components/todo';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route exact path="/" element={<Homepage />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/kanban" element={<TodoKanban />} />
+          <Route path="/list" element={<TodoList />} />
         </Routes>
       </Router>
     </main>
