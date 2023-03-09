42082d9774c13b753d64bf9d4ee69ba61f4b8fd2
joeygarber <joseph.z.garber@gmail.com>
2023-03-09 13:21:32 -0500

    Removed button that logs user

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -8,7 +8,6 @@ function Homepage () {
     <div className='homepage'>
       <h1>Homepage</h1>
       <p>This will be the landing page</p>
-      <button className='bg-blue-500 rounded-md p-1' onClick={() => console.log(user)}>Log user</button>
     </div>
   )
 }
