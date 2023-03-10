f5f9609129d5373899b7f4b5bc1eedd3624922a0
joeygarber <joseph.z.garber@gmail.com>
2023-03-10 07:17:35 -0500

    Added logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import fishinsea from '../assets/fishinsea.png'
 
 function Navbar() {
   return (
     <nav class="p-2 bg-white">
       <ul class="container flex flex-wrap items-center justify-between mx-auto border-4 border-black-500">
         <li class="flex justify-between items-center gap-12">
-          <img alt="AgenciFlow logo" />
-          <a class="font-bold text-xl">AgenciFlow</a>
+          <img src={fishinsea} className='w-6 m-1' alt="AgenciFlow logo" />
+          <h1 class="font-bold text-xl">AgenciFlow</h1>
         </li>
         <li class="flex justify-between items-center gap-12 mr-1 mb-1 p10">
           <Link to='/' class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
