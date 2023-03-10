0a8432d58499b36ad00b29aa680a127488e8a1db
joeygarber <joseph.z.garber@gmail.com>
2023-03-10 06:58:09 -0500

    Got Navbar links working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
-
   return (
     <nav class="p-2 bg-white">
       <ul class="container flex flex-wrap items-center justify-between mx-auto border-4 border-black-500">
@@ -16,36 +10,22 @@ function Navbar() {
           <a class="font-bold text-xl">AgenciFlow</a>
         </li>
         <li class="flex justify-between items-center gap-12 mr-1 mb-1 p10">
-          <Link href="#" class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
+          <Link to='/' class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
             Dashboard
           </Link>
-          <Link href="#" class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
+          <Link to='/chat' class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
             Chat
           </Link>
-          <Link href="#" class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
-            Projects
-          </Link>
-          <Link href="#" class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
+          <Link to='/clientmanagement' class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
             Clients
           </Link>
-          <Link href="#" class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
-            Task
+          <Link to='/list' class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
+            Task List
           </Link>
-          <Link href="#" class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
-            Board
-          </Link>
-          <Link href="#" class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
-            Invoices
+          <Link to='/kanban' class="border border-gray-300 hover:border-black cursor-pointer rounded outline-none">
+            Task Board
           </Link>
         </li>
-        <ul class="flex items-center">
-            <li>
-                <button onClick={toggleDropdown}>Profile</button>
-                {showDropdown && (
-                <button>Logout</button>
-                )}
-            </li>
-        </ul>
       </ul>
     </nav>
   );
